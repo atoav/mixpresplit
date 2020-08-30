a1eede8a8bff332800f9c24dfc403aeff7bbd9b2
David Huss <dh@atoav.com>
2020-08-30 21:21:24 +0200

    Add Test pre-commit hook

---
+++ scripts/run-tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+alias poetry='python3 /home/$USER/.poetry/bin/poetry'
+
+# if any command inside script returns error, exit and return that error
+set -e
+
+# magic line to ensure that we're always inside the root of our application,
+# no matter from which directory we'll run script
+# thanks to it we can just enter `./scripts/run-tests.sh`
+cd "${0%/*}/.."
+
+poetry run py.test
